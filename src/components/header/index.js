c712e322f09439bd45adf9429d1ac615f749cc28
alan santana <alan.nsantana@hotmail.com>
2020-11-20 12:39:24 -0300

    componente footer header e main

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import './header.css'
+
+export default function Header(){
+   return(
+       <header>
+           <nav>
+               <a href="#">Perfil</a>
+               <a href="#">Contato</a>
+               <a href="#">Produtos</a>
+           </nav>
+       </header>
+   )
+
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/header'
+import './global.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
